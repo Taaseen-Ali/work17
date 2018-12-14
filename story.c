5348e806173f3e1dd1df1e3c191c32aa2ec077da
Taaseen Ali <taaseen.ali89@gmail.com>
2018-12-13 23:08:09 -0500

    update

--- story.c
+++ story.c
@@ -8,26 +8,37 @@
 #include <unistd.h>
 #include <string.h>
 
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO
+			      (Linux-specific) */
+};
+
 int main(int argc, char * argv[]){
   key_t key;
   int shmid, semid, fd;
   char * data;
   key = ftok("file", 'R');
-
+  struct sembuf up = {.sem_num=0, .sem_op=1, .sem_flg=SEM_UNDO};
+  struct sembuf down = {.sem_num=0, .sem_op=-1, .sem_flg=SEM_UNDO};
+  
   if(argc > 1){
     if(strcmp(argv[1], "-c")){
       shmid = shmget(key, 200, 0644 | IPC_CREAT);
       data = shmat(shmid, (void *)0, 0);
       semid  = semget(key, 1, IPC_CREAT);
-      fd = open("story.txt", O_CREAT | O_TRUNC);
-    }
-    if(strcmp(argv[1], "-r")){
-      //stuff
-    }
-    if(strcmp(argv[1], "-v")){
-      //stuff
+      semctl(semid, 0, SETVAL, 1);
+	     fd = open("story.txt", O_CREAT | O_TRUNC);
+	     }
+	if(strcmp(argv[1], "-r")){
+	  //stuff
+	}
+      if(strcmp(argv[1], "-v")){
+	//stuff
+      }
     }
   }
-}
       
 
